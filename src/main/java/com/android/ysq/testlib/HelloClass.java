e1d38c2869893036d743852304dbcded6bc637b8
yangshengqing <340711340@qq.com>
2016-06-14 11:03:36 +0800

    初始化代码

---
+++ src/main/java/com/android/ysq/testlib/HelloClass.java
@@ -0,0 +1,10 @@
+package com.android.ysq.testlib;
+
+/**
+ * Created by ysq on 16/6/14.
+ */
+public class HelloClass {
+    public void sayHello() {
+        System.out.println("Hello World!");
+    }
+}
